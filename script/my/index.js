d0a1371486691f81561f4c32a0c64003f04f6f85
yuyang041060120 <15950456750@163.com>
2015-05-23 20:50:20 +0800

    创建store和action

---
+++ script/my/index.js
@@ -0,0 +1,39 @@
+var TodoActions = Reflux.createActions([
+    'getAll'
+]);
+
+var TodoStore = Reflux.createStore({
+    items: [],
+    listenables: [TodoActions],
+    onGetAll: function () {
+        this.items = [1, 2, 3];
+        this.trigger(this.items);
+    }
+});
+
+var TodoComponent = React.createClass({
+    mixins:[Reflux.listenTo(TodoStore,'onStatusChange')],
+    getInitialState: function () {
+        return {list: []};
+    },
+    onStatusChange: function () {
+        this.setState({list: TodoStore.items});
+    },
+    componentDidMount: function () {
+        //this.listenTo(TodoStore,this.onStatusChange);
+        ////TodoStore.listen(this.onStatusChange);
+        TodoActions.getAll();
+    },
+    render: function () {
+        return (
+            <div>
+                {this.state.list.map(function (item) {
+                    return <p>{item}</p>
+                })}
+            </div>
+        )
+    }
+});
+
+
+React.render(<TodoComponent />, document.getElementById('container'));
